094e549e904ef0829645750348361bd98efa6fb6
Rohit Patnaik <quanticle@quanticle.net>
2018-09-27 22:34:54 -0500

    Translate input filenames to output filenames.

--- ssgn/ssgn.py
+++ ssgn/ssgn.py
@@ -1,8 +1,13 @@
-import pypandoc
+import glob
+from itertools import chain
+import os
+
 from jinja2 import Environment, FileSystemLoader
+import pypandoc
+
+def get_output_path(source_dir, source_path, destination_dir):
+    return os.path.join(destination_dir, os.path.relpath(source_path, start=source_dir))
 
 def convert_to_html(markdown_string):
     return pypandoc.convert_text(markdown_string, "html", format="md")
 
-def render_template(input_text, template):
-    pass
--- ssgn_tests/ssgn_tests.py
+++ ssgn_tests/ssgn_tests.py
@@ -6,6 +6,7 @@ from unittest.mock import (
 
 from ssgn.ssgn import (
     convert_to_html,
+    get_output_path
 )
 
 class TestSSGN:
@@ -14,3 +15,13 @@ class TestSSGN:
     def test_convert_to_html(self, patched_convert_text):
         output = convert_to_html("# test markdown")
         patched_convert_text.assert_called_once_with("# test markdown", "html", format="md")
+
+    def test_get_output_filename_flat(self):
+        output_filename = get_output_path("input", "input/foo.md", "output")
+        assert output_filename == "output/foo.md"
+
+    def test_get_output_filename_nested(self):
+        output_filename = get_output_path("input", "input/foo/bar/test.md", "output")
+        assert output_filename == "output/foo/bar/test.md"
+
+    
